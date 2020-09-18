4dc7accb0e068315b1c1447cd39a2af4e2cb7966
acidepicice <2021arao@tjhsst.edu>
2020-09-18 00:17:32 -0400

    Log request session

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,6 @@
 module.exports.set = function(app){
     app.get('/', (req, res) => {
+        console.log(req.session);
         res.render('index', {user: req.session ? req.session.display_name : ''});
     });
 }
