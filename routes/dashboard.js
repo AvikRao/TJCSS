f2ca2b67c5b18ca3ea2f554a53792b51d9f4a204
AvikRao <2021arao@tjhsst.edu>
2020-09-18 16:49:13 -0400

    Clean up hbs variable serve

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,5 +1,10 @@
 module.exports.set = function (app) {
     app.get('/dashboard', (req, res) => {
-        res.render('dashboard', { user: req.session ? req.session.display_name : '' });
+
+        if (req.session) {
+            res.render('/dashboard', { user: req.session ? req.session : null });
+        } else {
+            res.redirect('/');
+        }
     });
 }
