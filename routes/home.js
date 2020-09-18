384805438a0ffb61ba4510dcb1de39ddf4a2c8c9
ladsfsdfs <nguyen.k.allen@gmail.com>
2020-09-18 08:23:32 -0400

    mucking with cookie middleware

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,11 @@
-module.exports.set = function(app){
+module.exports.set = function (app) {
     app.get('/', (req, res) => {
+        app.set('trust proxy', 1)
+        app.use(cookieSession({
+            name: 'peppermint',
+            keys: ['director Broke have a nice day', '1123xXx_pHasZe_qu1ckSc0p3r_xXx1123']
+        }))
         console.log(req.session);
-        res.render('index', {user: req.session ? req.session.display_name : ''});
+        res.render('index', { user: req.session ? req.session.display_name : '' });
     });
 }
