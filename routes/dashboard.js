277f93f400e0f91957b74630ae92873f1c85808e
AvikRao <2021arao@tjhsst.edu>
2020-09-18 16:56:58 -0400

    Revert variable serve

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,7 +2,7 @@ module.exports.set = function (app) {
     app.get('/dashboard', (req, res) => {
 
         if (req.session) {
-            res.render('dashboard', { user: req.session ? req.session : null });
+            res.render('dashboard', { user: req.session ? req.session.display_name : '' });
         } else {
             res.redirect('/');
         }
