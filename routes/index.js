940706a3861177ae64d0d657c6b90404fbbfd869
acidepicice <2021arao@tjhsst.edu>
2020-09-18 00:32:21 -0400

    Switch middleware ordering

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ let login = require('./login')
 let signup = require('./signup')
 
 module.exports.set = function(app){
-    home.set(app);
     login.set(app);
+    home.set(app);
     signup.set(app);
 }
