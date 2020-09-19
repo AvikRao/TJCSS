ed343abd88ee2f7d80369d0297efc26367f0a1a1
AvikRao <2021arao@tjhsst.edu>
2020-09-19 00:01:02 -0400

    Design dashboard page

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,5 @@
 module.exports.set = function (app) {
     app.get('/', (req, res) => {
-        console.log(req.session);
         res.render('index', { user: req.session ? (req.session.exists ? req.session : false) : false });
     });
 }
