fc4b1e2abbedd6082f6a4372d34b80a24a6a454d
AvikRao <2021arao@tjhsst.edu>
2020-09-26 00:00:44 -0400

    Fix console log statement for debug

--- routes/login.js
+++ routes/login.js
@@ -76,7 +76,7 @@ module.exports.set = function(app){
             req.session.is_teacher = resp.data.is_teacher;
             req.session.exists = true;
 
-            console.log(session);
+            console.log(req.session);
 
             let users = await db.query('SELECT * FROM users WHERE id=%s;', [req.session.userid]);
 
